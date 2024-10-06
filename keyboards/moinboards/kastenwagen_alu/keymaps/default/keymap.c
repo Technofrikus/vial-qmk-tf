d08b7c46d573cb1d415fe9a0d41361c2ad8c8fbb
Technofrikus <technofrikus@gmx.net>
2024-10-06 14:34:39 +0200

    Kastewagen Alu firmware update

--- keyboards/moinboards/kastenwagen_alu/keymaps/default/keymap.c
+++ keyboards/moinboards/kastenwagen_alu/keymaps/default/keymap.c
@@ -19,14 +19,6 @@
 
 #include QMK_KEYBOARD_H
 
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-    //                  Encoder 1                                     Encoder 2
-};
-#endif
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_BSLS,          KC_P7,   KC_P8,   KC_P9,   KC_PPLS,  //17
