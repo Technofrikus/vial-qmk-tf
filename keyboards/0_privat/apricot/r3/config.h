eefa1954a3ae852b920ec6edab3542b5ef76272a
Technofrikus <technofrikus@gmx.net>
2026-04-26 10:58:37 +0200

    Added some lazydesigner boards to my private folder

---
+++ keyboards/0_privat/apricot/r3/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define WS2812_DI_PIN C13
+#define RGBLED_NUM 4         
+
+#define RGBLIGHT_ANIMATIONS
+
+#define RGBLIGHT_EFFECT_BREATHING      // 呼吸灯
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD   // 彩虹 mood
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL  // 彩虹 swirl
+#define RGBLIGHT_EFFECT_SNAKE          // 蛇形
+#define RGBLIGHT_EFFECT_KNIGHT         // 骑士跑马灯
+#define RGBLIGHT_EFFECT_CHRISTMAS      // 圣诞
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT// 静态渐变
+#define RGBLIGHT_EFFECT_RGB_TEST       // RGB 测试
+#define RGBLIGHT_EFFECT_ALTERNATING    // 交替
+#define RGBLIGHT_EFFECT_TWINKLE        // 闪烁
+
+
+#define MCU STM32F072
---
+++ keyboards/0_privat/apricot/r3/readme.md
@@ -0,0 +1,18 @@
+# apricot R3
+
+A qaz keyboard designed and produced in 2024 by [LAZYDESIGNERS](http://lazydesigners.cn).
+
+* Keyboard Maintainer: [LAZYDESIGNERS](https://github.com/jackytrabbit)
+* Hardware Supported: apricot r3
+* Hardware Availability: Check [LAZYDESIGNERS's homepage.](http://lazydesigners.cn)
+
+Make example for this keyboard (after setting up your vial build environment):
+
+    make lazydesigners/apricot/rev2:vial
+
+Flashing example for this keyboard:
+
+    make lazydesigners/apricot/rev2/vial:flash
+
+**Reset Key:** To enter the bootloader, push the boot button and short RESET pins on the pcb.
+
